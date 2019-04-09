c4852ceb8987ed3ab7b4eb7cc7158949b3dd7ccd
João Gabriel Jardim <joaogabrieljardim@Joaos-MacBook-Air.local>
2019-04-08 22:31:32 -0300

    Adiciona algumas tabelas verdades.

---
+++ modalvalidator.py
@@ -0,0 +1,32 @@
+
+def is_terminal(exp) :
+    if exp == 'T' or exp == 'F' :
+        return True
+    else :
+        return False
+
+def not_func(op)
+    if op == 'T':
+        return 'F'
+    else :
+        return 'T'
+
+def or_func(ls, rs) :
+    if ls == 'T' or rs == 'T' :
+        return 'T'
+    else :
+        return 'F'
+
+def or_and(ls, rs) :
+    if ls == 'T' and rs == 'T' :
+        return 'T'
+    else :
+        return 'F'
+
+
+
+// process_all_nots
+//process_e
+//process
+
+print(or_func('F', 'F'))
