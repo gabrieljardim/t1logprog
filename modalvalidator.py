fbd41124aae623a272d54cbde5606191bdc63ac9
João Gabriel Jardim <joaogabrieljardim@Joaos-MacBook-Air.local>
2019-04-10 01:09:42 -0300

    Adiciona validação do conectivo not e substitui expressão inicial pelo valor da expressão resolvida.

--- modalvalidator.py
+++ modalvalidator.py
@@ -1,11 +1,10 @@
-
 def is_terminal(exp) :
     if exp == 'T' or exp == 'F' :
         return True
     else :
         return False
 
-def not_func(op)
+def not_func(op):
     if op == 'T':
         return 'F'
     else :
@@ -23,10 +22,51 @@ def or_and(ls, rs) :
     else :
         return 'F'
 
+def find_not_token(expression):
+    return expression.find('!')
+
+def process_not_token(position, expression, variableSet):
+    if (position+1) < len(expression) :
+        variableValue = variableSet[expression[position+1]]
+        expression_first = expression[:position]
+        expression_second = expression[position+2:]
+        return (expression_first + not_func(variableValue) + expression_second)
 
 
-// process_all_nots
-//process_e
-//process
 
-print(or_func('F', 'F'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#// process_all_nots
+#//process_e
+#//process
+
+variableSet = {
+    'p':'T',
+    'q':'F'
+}
+
+expression = "p|q&!p"
+
+
+pos = find_not_token(expression)
+if pos!=-1:
+    resultStr = process_not_token(pos, expression, variableSet)
+
+print(expression)
+print(resultStr)
+
+
+
+#print(or_func('F', 'F'))
