95b5c43e2b2e1ac816405eab0d31a9f4d477063c
João Gabriel Jardim <joaogabrieljardim@Joaos-MacBook-Air.local>
2019-04-12 02:28:10 -0300

    Reconhece expressões com not, and, or, if then, nessa ordem de precedência.

--- modalvalidator.py
+++ modalvalidator.py
@@ -4,11 +4,17 @@ def is_terminal(exp) :
     else :
         return False
 
-def not_func(op):
-    if op == 'T':
+def not_func(rs):
+    if rs == 'T':
         return 'F'
     else :
         return 'T'
+
+def and_func(ls, rs) :
+    if ls == 'T' and rs == 'T' :
+        return 'T'
+    else :
+        return 'F'
 
 def or_func(ls, rs) :
     if ls == 'T' or rs == 'T' :
@@ -16,57 +22,107 @@ def or_func(ls, rs) :
     else :
         return 'F'
 
-def or_and(ls, rs) :
-    if ls == 'T' and rs == 'T' :
-        return 'T'
-    else :
+def ifthen_func(ls, rs) :
+    if ls == 'T' and rs == 'F' :
         return 'F'
+    else :
+        return 'T'
 
-def find_not_token(expression):
-    return expression.find('!')
 
-def process_not_token(position, expression, variableSet):
+def evaluate_not_token(position, expression, variableSet):
     if (position+1) < len(expression) :
         variableValue = variableSet[expression[position+1]]
         expression_first = expression[:position]
         expression_second = expression[position+2:]
         return (expression_first + not_func(variableValue) + expression_second)
 
+def evaluate_and_token(position, expression, variableSet):
+    if (position+1) < len(expression) and (position-1) >= 0 :
+        right_side_variable_value = variableSet[expression[position+1]]
+        left_side_variable_value = variableSet[expression[position-1]]
+        expression_first = expression[:position-1]
+        expression_second = expression[position+2:]
+        return (expression_first + and_func(left_side_variable_value, right_side_variable_value) + expression_second)
 
+def evaluate_or_token(position, expression, variableSet):
+    if (position+1) < len(expression) and (position-1) >= 0 :
+        right_side_variable_value = variableSet[expression[position+1]]
+        left_side_variable_value = variableSet[expression[position-1]]
+        expression_first = expression[:position-1]
+        expression_second = expression[position+2:]
+        return (expression_first + or_func(left_side_variable_value, right_side_variable_value) + expression_second)
 
+def evaluate_ifthen_token(position, expression, variableSet):
+    if (position+2) < len(expression) and (position-1) >= 0 :
+        right_side_variable_value = variableSet[expression[position+2]]
+        left_side_variable_value = variableSet[expression[position-1]]
+        expression_first = expression[:position-1]
+        expression_second = expression[position+3:]
+        return (expression_first + ifthen_func(left_side_variable_value, right_side_variable_value) + expression_second)
 
+def find_not_token(expression):
+    return expression.find('!')
 
+def find_and_token(expression):
+    return expression.find('&')
 
+def find_or_token(expression):
+    return expression.find('|')
 
+def find_ifthen_token(expression):
+    return expression.find('->')
 
+def process_not(expression, variableSet):
+    while(True):
+        pos = find_not_token(expression)
+        if pos == -1:
+            break
+        expression = evaluate_not_token(pos, expression, variableSet)
+    return expression
 
+def process_and(expression, variableSet):
+    while(True):
+        pos = find_and_token(expression)
+        if pos == -1:
+            break
+        expression = evaluate_and_token(pos, expression, variableSet)
+    return expression
 
+def process_or(expression, variableSet):
+    while(True):
+        pos = find_or_token(expression)
+        if pos == -1:
+            break
+        expression = evaluate_or_token(pos, expression, variableSet)
+    return expression
 
-
-
-
-
-
-
-#// process_all_nots
-#//process_e
-#//process
+def process_ifthen(expression, variableSet):
+    while(True):
+        pos = find_ifthen_token(expression)
+        if pos == -1:
+            break
+        expression = evaluate_ifthen_token(pos, expression, variableSet)
+    return expression
 
 variableSet = {
     'p':'T',
-    'q':'F'
+    'q':'F',
+
+    #do not modify
+    'F':'F',
+    'T':'T'
 }
 
-expression = "p|q&!p"
+expression = "p|q&!p->q&!q->p"
 
 
-pos = find_not_token(expression)
-if pos!=-1:
-    resultStr = process_not_token(pos, expression, variableSet)
+expression = process_not(expression, variableSet)
+print("EtapaNot: " + expression)
+expression = process_and(expression, variableSet)
+print("EtapaAnd: " + expression)
+expression = process_or(expression, variableSet)
+print("EtapaOr: " + expression)
+expression = process_ifthen(expression, variableSet)
+print("EtapaIfthen: " + expression)
 
 print(expression)
-print(resultStr)
-
-
-
-#print(or_func('F', 'F'))
